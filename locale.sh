18b4b9f7b92494638061f4c919f3b3f0f291011e
Tarasenko Dmitriy <70448099+Poloxin@users.noreply.github.com>
2022-04-07 13:55:14 +0400

    Delete locale.sh

--- locale.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fix Locale
-echo -e "--------------Fix locale settings!----------------"
-
-export LC_ALL="en_US.UTF-8"
-locale-gen "en_US.UTF-8"
-
-echo -e "--------------All done!---------------"
