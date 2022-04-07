dc52942ff8c5eb29b723b00b92d9464cd3e6f57c
poloxin <dima.tarasenko.03@inbox.ru>
2022-04-07 14:17:35 +0400

    Well done!

--- main.sh
+++ main.sh
@@ -5,12 +5,12 @@ echo -e "Start Script!\n"
 
 
 # Fix Locale
-echo -e "Fix locale settings!"
+#echo -e "Fix locale settings!"
 
-export LC_ALL="en_US.UTF-8" 
-locale-gen "en_US.UTF-8" 
+#export LC_ALL="en_US.UTF-8" 
+#locale-gen "en_US.UTF-8" 
 
-echo -e "Done!\n"
+#echo -e "Done!\n"
 
 
 # Install Nextcloud
