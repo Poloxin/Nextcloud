4e33157ef3973f3761d26dd097637a1c50a59980
poloxin <dima.tarasenko.03@inbox.ru>
2022-04-08 16:34:43 +0400

    new

--- blocks/php.sh
+++ blocks/php.sh
@@ -2,7 +2,7 @@
 
 # Config PHP
 echo -e "\n----PHP----"
-sed 's/#env[HOSTNAME] = $HOSTNAME/env[HOSTNAME] = $HOSTNAME/w' /etc/php/7.4/fpm/pool.d/www.conf
+sed -ne 's/\;env[HOSTNAME] = $HOSTNAME/env[HOSTNAME] = $HOSTNAME' /etc/php/7.4/fpm/pool.d/www.conf
 
 systemctl enable php7.4-fpm &> /dev/null
 systemctl restart php7.4-fpm &> /dev/null
