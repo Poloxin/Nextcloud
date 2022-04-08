16307767d3457cb07043466e59a963da72ade722
poloxin <dima.tarasenko.03@inbox.ru>
2022-04-08 21:22:07 +0400

    Up

--- blocks/installn.sh
+++ blocks/installn.sh
@@ -2,5 +2,8 @@
 
 # Install programs
 echo -e "\n--Install--"
-apt install -y htop mariadb-server mariadb-client php php-gmp php-bcmath php-fpm php-common php-zip php-xml php-intl php-gd php-mysql php-mbstring php-curl php-imagick php-ldap php-json php-opcache php-apcu nginx unzip &> /dev/null
+apt install -y htop mariadb-server mariadb-client php php-gmp php-bcmath php-fpm php-common php-zip php-xml php-intl php-gd php-mysql php-mbstring php-curl php-imagick php-ldap php-json php-opcache php-apcu nginx unzip &> /dev/null gnupg2 gnupg1 gnupg redis-server postgresql php-redis memcached php-memcached rabbitmq-server nginx-extras
+
+systemctl enable postgresql &> /dev/null
+systemctl enable memcached &> /dev/null
 echo -e "----Done----\n"
--- blocks/php.sh
+++ blocks/php.sh
@@ -2,7 +2,21 @@
 
 # Config PHP
 echo -e "\n----PHP----"
-sed -ne 's/\;env[HOSTNAME] = $HOSTNAME/env[HOSTNAME] = $HOSTNAME' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "/^\;env\[HOSTNAME\] = \$HOSTNAME/s/\;//" /etc/php/7.4/fpm/pool.d/www.conf 
+sed -i "/^\;env\[PATH\] = \/usr\/local\/bin:\/usr\/bin:\/bin/s/\;//" /etc/php/7.4/fpm/pool.d/www.conf 
+sed -i "/^\;env\[TMP\] = \/tmp/s/\;//" /etc/php/7.4/fpm/pool.d/www.conf 
+sed -i "/^\;env\[TMPDIR\] = \/tmp/s/\;//" /etc/php/7.4/fpm/pool.d/www.conf 
+sed -i "/^\;env\[TEMP\] = \/tmp/s/\;//" /etc/php/7.4/fpm/pool.d/www.conf 
+
+sed -i "/^\;opcache.enable=/s/\;//" /etc/php/7.4/fpm/php.ini
+sed -i "s/\;opcache.enable_cli=0/opcache.enable_cli=1/" /etc/php/7.4/fpm/php.ini
+sed -i "/^\;opcache.interned_strings_buffer=8/s/\;//" /etc/php/7.4/fpm/php.ini
+sed -i "/^\;opcache.max_accelerated_files=10000/s/\;//" /etc/php/7.4/fpm/php.ini
+sed -i "/^\;opcache.memory_consumption=128/s/\;//" /etc/php/7.4/fpm/php.ini
+sed -i "/^\;opcache.save_comments=1/s/\;//" /etc/php/7.4/fpm/php.ini
+sed -i "s/\;opcache.revalidate_freq=2/opcache.revalidate_freq=1/" /etc/php/7.4/fpm/php.ini
+sed -i "/^\;memory_limit/s/128M/512M/" /etc/php/7.4/fpm/php.ini
+
 
 systemctl enable php7.4-fpm &> /dev/null
 systemctl restart php7.4-fpm &> /dev/null
