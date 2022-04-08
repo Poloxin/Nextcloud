a3e1a0a8b2c7ad7e5c95347f295b82a5a4a0d6f1
poloxin <dima.tarasenko.03@inbox.ru>
2022-04-08 22:18:28 +0400

    Upg

--- blocks/upgraden.sh
+++ blocks/upgraden.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # Upgrade Nextcloud
+sed -i "$s/i\ 'memcache.local' => '\OC\Memcache\APCu',
+ 'memcache.distributed' => '\OC\Memcache\Redis',
+   'redis' =>
+   array (
+        'host' => '127.0.0.1',
+        'port' => 6379,
+   ),
+   'memcache.locking' => '\OC\Memcache\Redis',/" /var/www/nextcloud/config/config.php
