6c2a3a510000f12d072e4ec64cbd437514204230
poloxin <dima.tarasenko.03@inbox.ru>
2022-04-09 09:34:02 +0400

    Re

--- blocks/upgraden.sh
+++ blocks/upgraden.sh
@@ -2,9 +2,13 @@
 
 # Upgrade Nextcloud
 echo -e "\n-Upgrade-NC-"
-sed -i  "$ i\  \'memcache.local\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.distributed\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.locking\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'redis\' => \n      array (\n          \'host\' => \'localhost\',          \'port\' => 6379,\n      )," /var/www/nextcloud/config/config.php
+sed -i  "$ i\  \'memcache.local\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.distributed\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.locking\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'redis\' => \n      array (\n          \'host\' => \'localhost\',\n          \'port\' => 6379,\n      )," /var/www/nextcloud/config/config.php
 sed -i  "$ i\  \  \'memcache.local\' => \'\\\\\OC\\\\\Memcache\\\\\Memcached\',\n  \'memcache.distributed\' => \'\\\\\OC\\\\\Memcache\\\\\Memcached\',\n  \'memcached_servers\' =>\n  array (\n    0 =>\n    array (\n      0 => \'localhost\',\n      1 => 11211,\n    ),\n  )," /var/www/nextcloud/config/config.php
 
 sed -i "$ i\  \'default_phone_region\' => \'RU\'," /var/www/nextcloud/config/config.php
-sed -i "8 a\    \'192.168.1.107:8080\'," /var/www/nextcloud/config/config.php
+sed -i "8 a\    1 => \'192.168.1.107:8080\'," /var/www/nextcloud/config/config.php
+
+sudo -u www-data php /var/www/nextcloud/occ app:install onlyoffice
+sudo -u www-data php /var/www/nextcloud/occ app:enable user_ldap
+#sudo -u www-data php /var/www/nextcloud/occ app:disable dashboard
 echo -e "----Done----\n"
