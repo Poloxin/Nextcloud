e8154c53527056e6c0e03222aabb04e8dd4c78f3
poloxin <dima.tarasenko.03@inbox.ru>
2022-04-09 09:05:36 +0400

    Re

--- blocks/upgraden.sh
+++ blocks/upgraden.sh
@@ -2,10 +2,7 @@
 
 # Upgrade Nextcloud
 echo -e "\n-Upgrade-NC-"
-sed -i  "$ i\  \'memcache.local\' => \'\OC\Memcache\APCu\',\n \'memcache.distributed\' => \'\OC\Memcache\Redis\',\n   \'redis\' =>\n   array (\n        \'host\' => '127.0.0.1',\n        \'port\' => 6379,\n   ),\n   \'memcache.locking\' => \'\OC\Memcache\Redis\',/" /var/www/nextcloud/config/config.php
-sed -i "$ i\  \'default_phone_region\' => \'RU\',/" /var/www/nextcloud/config/config.php
-sed -ie "/^\    0 => \'192.168.1.107\'/a \    \'192.168.1.107:8080\',"
-
-echo -e "sudo -u www-data php /var/www/nextcloud/occ files:scan --all"
-echo -e "sudo -u www-data php /var/www/nextcloud/occ db:convert-filecache-bigint"
+sed -i  "$ i\"  \'memcache.local\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.distributed\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.locking\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'redis\' => \n      array (\n          \'host\' => \'localhost\',          \'port\' => 6379,\n      )," /var/www/nextcloud/config/config.php
+sed -i "$ i\  \'default_phone_region\' => \'RU\'," /var/www/nextcloud/config/config.php
+sed -i "8 a\    \'192.168.1.107:8080\'," /var/www/nextcloud/config/config.php
 echo -e "----Done----\n"
