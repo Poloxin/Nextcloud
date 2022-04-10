69821d7dd58e59e8ce68a4a7a038fc5fd8263f17
poloxin <dima.tarasenko.03@inbox.ru>
2022-04-10 12:50:22 +0400

    Upgrade

--- blocks/upgraden.sh
+++ blocks/upgraden.sh
@@ -2,6 +2,8 @@
 
 # Upgrade Nextcloud
 echo -e "\n-Upgrade-NC-"
+cd /var/www/nextcloud/
+sudo -u www-data php occ maintenance:install --databasei "mariadb" --database-name "nextcloud"  --database-user "nextcloud" --database-pass "nextcloud" --admin-user "admin" --admin-pass "macinsoft"
 sed -i  "$ i\  \'memcache.local\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.distributed\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'memcache.locking\' => \'\\\\\OC\\\\\Memcache\\\\\Redis\',\n  \'redis\' => \n      array (\n          \'host\' => \'localhost\',\n          \'port\' => 6379,\n      )," /var/www/nextcloud/config/config.php
 sed -i  "$ i\  \  \'memcache.local\' => \'\\\\\OC\\\\\Memcache\\\\\Memcached\',\n  \'memcache.distributed\' => \'\\\\\OC\\\\\Memcache\\\\\Memcached\',\n  \'memcached_servers\' =>\n  array (\n    0 =>\n    array (\n      0 => \'localhost\',\n      1 => 11211,\n    ),\n  )," /var/www/nextcloud/config/config.php
 
@@ -19,5 +21,14 @@ sudo -u www-data php /var/www/nextcloud/occ app:install side_menu
 sudo -u www-data php /var/www/nextcloud/occ app:disable weather_status
 sudo -u www-data php /var/www/nextcloud/occ app:disable photos
 sudo -u www-data php /var/www/nextcloud/occ app:enable user_ldap
-#sudo -u www-data php /var/www/nextcloud/occ app:disable dashboard
+
+webuser=www-data
+sudo -u ${webuser} php /var/www/nextcloud/occ ldap:delete-config s01
+sudo -u ${webuser} php /var/www/nextcloud/occ ldap:create-empty-config
+sudo -u ${webuser} php /var/www/nextcloud/occ ldap:set-config s01 ldapHost "192.168.1.2"
+sudo -u ${webuser} php /var/www/nextcloud/occ ldap:set-config s01 ldapPort "389"
+sudo -u ${webuser} php /var/www/nextcloud/occ ldap:set-config s01 ldapAgentName "sup"
+sudo -u ${webuser} php /var/www/nextcloud/occ ldap:set-config s01 ldapAgentPassword "AstraPass"; history -d $((HISTCMD-1))
+sudo -u ${webuser} php /var/www/nextcloud/occ ldap:set-config s01 ldapBase "dc=tpc,dc=tpcol,dc=ru"
+
 echo -e "----Done----\n"
